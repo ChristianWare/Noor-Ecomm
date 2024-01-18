0dcdbfc7a4e950f22faf2762041876b95381322b
Chris Ware <c.p.ware@hotmail.com>
2024-01-18 14:57:04 -0700

    Connected Sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,19 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './src/schemas'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./src/schemas";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
-  name: 'default',
-  title: 'noor ecomm',
-
-  projectId: 't50e6ck4',
-  dataset: 'production',
-
+  basePath: "/studio",
+  name: "OREBI_Shopping",
+  title: "noor ecomm",
+  projectId,
+  dataset,
   plugins: [structureTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
-})
+});
